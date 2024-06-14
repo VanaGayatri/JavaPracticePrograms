0609633e6ed5e729a3314d67019b5a9700114dae
Vana Gayatri <vanagayatri@gmail.com>
2024-06-14 20:38:17 +0530

    Formatted

--- Java_Practice/src/com/java/practice/FibonacciSeries.java
+++ Java_Practice/src/com/java/practice/FibonacciSeries.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class FibonacciSeries {
 
 	public static void main(String[] args) {
-		// Number of Fibonacci numbers to generate		
+		// Number of Fibonacci numbers to generate
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the number of Fibonacci numbers to generate: ");
 		int n = sc.nextInt();
@@ -13,43 +13,43 @@ public class FibonacciSeries {
 		printFibonacciUpToGivenNterms(n);
 		printFibonacciUpToGivenRange(n);
 	}
-	
+
 	public static void printFibonacciUpToGivenNterms(int n) {
-		if(n<=0) {
+		if (n <= 0) {
 			System.out.println("Please enter a positive interger.");
 			return;
 		}
-		int a=0;
-		int b=1;
+		int a = 0;
+		int b = 1;
 		System.out.print(a);
-		if(n>1)
-			System.out.print(" "+b);
-		for(int i=2;i<n;i++) {
-			int next =a+b;
-			System.out.print(" "+next);
-			a=b;
-			b=next;
+		if (n > 1)
+			System.out.print(" " + b);
+		for (int i = 2; i < n; i++) {
+			int next = a + b;
+			System.out.print(" " + next);
+			a = b;
+			b = next;
 		}
 	}
-	
+
 	public static void printFibonacciUpToGivenRange(int n) {
-		if(n<=0) {
+		if (n <= 0) {
 			System.out.println("Please enter a positive interger.");
 			return;
 		}
-		int a=0;
-		int b=1;
+		int a = 0;
+		int b = 1;
 		System.out.println();
 		System.out.print(a);
-		if(n >= 1)
-			System.out.print(" "+b);
-		while(true) {
-			int next =a+b;
-			if(next > n)
+		if (n >= 1)
+			System.out.print(" " + b);
+		while (true) {
+			int next = a + b;
+			if (next > n)
 				break;
-			System.out.print(" "+next);
-			a=b;
-			b=next;
+			System.out.print(" " + next);
+			a = b;
+			b = next;
 		}
 	}
 
--- Java_Practice/src/com/java/practice/PrimeNumber.java
+++ Java_Practice/src/com/java/practice/PrimeNumber.java
@@ -8,40 +8,48 @@ public class PrimeNumber {
 		// check if given number is Prime
 		System.out.println("Please enter a number to check if its Prime or Composite : ");
 		Scanner sc = new Scanner(System.in);
-		
-		while(!sc.hasNextInt()) {
+
+		while (!sc.hasNextInt()) {
 			System.out.println("Please enter a valid integer : ");
 			sc.next();
 			System.out.println("Please enter a number to check if its Prime or Composite : ");
 		}
 		int n = sc.nextInt();
 		sc.close();
-		
-		if(n<0) {
+
+		if (n < 0) {
 			System.out.println("Please Enter a positive Number");
-		}else if(n==0 || n==1) {
-			System.out.println(n+" is a neither Prime nor Composite.");
-		}else if(isPrime(n) && isPrimeOrNot(n)) {
-			System.out.println(n +" is a Prime Number");			
-		}else {
-			System.out.println(n+" is a composite number.");			
+		} else if (n == 0 || n == 1) {
+			System.out.println(n + " is a neither Prime nor Composite.");
+		} else if (isPrime(n) && isPrimeOrNot(n)) {
+			System.out.println(n + " is a Prime Number");
+		}
+//		else if (isPrimeOrNot(n)) {
+//			System.out.println(n + " is a Prime Number");
+//		}
+		else {
+			System.out.println(n + " is a composite number.");
 		}
 	}
-	
+
 	// general
 	public static boolean isPrime(int n) {
-		for(int i=2; i<=Math.sqrt(n);i++) {
-			if(n%i ==0) return false;
+		for (int i = 2; i <= Math.sqrt(n); i++) {
+			if (n % i == 0)
+				return false;
 		}
 		return true;
 	}
-	
+
 	// more efficient
 	public static boolean isPrimeOrNot(int n) {
-		if(n<=3) return true;
-		if(n%2==0 || n%3==0) return false;
-		for(int i=2; i*i<=n; i+=6) {
-			if(n%i ==0) return false;
+		if (n <= 3)
+			return true;
+		if (n % 2 == 0 || n % 3 == 0)
+			return false;
+		for (int i = 2; i * i <= n; i += 6) {
+			if (n % i == 0)
+				return false;
 		}
 		return true;
 	}
